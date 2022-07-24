cad8f224604f2c91683de38a38db9993eded4b28
janardan001157 <janardanchaudhary001@gmail.com>
2022-07-24 15:31:22 +0530

    leetcode 240

---
+++ src/leetcode/Problem240.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+public class Problem240 {
+
+    int rowSize;
+    int colSize;
+
+    public static void main(String[] args) {
+        int[][] matrix =
+                {{1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}};
+        Problem240 problem240 = new Problem240();
+        System.out.println(problem240.searchMatrix(matrix, 8));
+//        System.out.println(problem240.searchMatrix(matrix, 19));
+//        System.out.println(problem240.searchMatrix(matrix, 23));
+//        System.out.println(problem240.searchMatrix(matrix, 18));
+//        System.out.println(problem240.searchMatrix(matrix, 123));
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        rowSize = matrix.length;
+        colSize = matrix[0].length;
+        return checkIfPresent(matrix, target, 0, 0);
+    }
+
+    public boolean checkIfPresent(int[][] matrix, int target, int rowIndex, int colIndex) {
+        if(rowIndex >= rowSize && colIndex >= colSize) {
+            return false;
+        }
+        if(rowIndex < rowSize && colIndex < colSize) {
+            if(matrix[rowIndex][colIndex] == target) {
+                return true;
+            } else {
+                return checkIfPresent(matrix, target, rowIndex+1, colIndex+1);
+            }
+        } else if(rowIndex < rowSize) {
+            return checkIfPresent(matrix, target, rowIndex, colIndex-1);
+        } else {
+            return checkIfPresent(matrix, target, rowIndex-1, colIndex);
+        }
+    }
+
+//    public boolean leftCheck(int[][] matrix, int target, int rowIndex, int colIndex) {
+//        if(rowIndex < rowSize )
+//    }
+}
